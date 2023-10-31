ec145dd4ad83ba0e1dc4d9f4010feaf361a4bf73
Chang Yang <yangchang@deepin.org>
2023-10-31 11:18:37 +0800

    feat: init package

---
+++ src/libkdepim/progresswidget/progressstatusbarwidget.cpp
@@ -0,0 +1,40 @@
+/*
+   SPDX-FileCopyrightText: 2014-2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+#include "progressstatusbarwidget.h"
+
+#include "progressdialog.h"
+#include "statusbarprogresswidget.h"
+
+using namespace KPIM;
+class KPIM::ProgressStatusBarWidgetPrivate
+{
+public:
+    ProgressStatusBarWidgetPrivate() = default;
+
+    KPIM::StatusbarProgressWidget *mLittleProgress = nullptr;
+};
+
+ProgressStatusBarWidget::ProgressStatusBarWidget(QWidget *alignWidget, QWidget *parent, unsigned int showTypeProgressItem)
+    : QObject(parent)
+    , d(new KPIM::ProgressStatusBarWidgetPrivate)
+{
+    auto progressDialog = new KPIM::ProgressDialog(alignWidget, parent);
+    progressDialog->setObjectName(QStringLiteral("progressDialog"));
+    progressDialog->setShowTypeProgressItem(showTypeProgressItem);
+    progressDialog->hide();
+
+    d->mLittleProgress = new KPIM::StatusbarProgressWidget(progressDialog, alignWidget);
+    d->mLittleProgress->setObjectName(QStringLiteral("littleprogress"));
+    d->mLittleProgress->setShowTypeProgressItem(showTypeProgressItem);
+    d->mLittleProgress->show();
+}
+
+ProgressStatusBarWidget::~ProgressStatusBarWidget() = default;
+
+KPIM::StatusbarProgressWidget *ProgressStatusBarWidget::littleProgress() const
+{
+    return d->mLittleProgress;
+}
